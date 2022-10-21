043607f352598629208eb3446ff274d45067486e
Klubuntu <local@fix.pc>
2022-10-22 00:02:09 +0200

    First public release

---
+++ README.md
@@ -0,0 +1,12 @@
+# Librus CLI
+### To run this program, you need installed node.js > 12.0
+
+## Install Node.js
+> Method One - https://nodejs.org/en/
+
+> Method Two (use powershell/cmd, only works on Windows 10/11) winget install node.js
+
+## Run this program
+> Open terminal/console on directory
+
+Type command: *node app.js*
---
+++ app.js
@@ -0,0 +1,54 @@
+const { Table } = require("console-table-printer");
+const Librus = require('./API/api')
+
+const { login, pass } = require("./config");
+
+const api = new Librus();
+
+function welcome(){
+    console.log("Librus CLI v0.x\n\n")
+    console.log("Loading Default Option - Available Grades Table")
+}
+
+async function getGradesTable(token){
+    const usrGrades = await api.getGrades(token).then(data => { return data });
+    let vp
+
+    vp = new Table({
+        columns: [
+          { name: "Grade", alignment: "center", color: "green" }, //
+          { name: "Lesson", alignment: "center", color: "yellow" },
+          { name: "Teacher", alignment: "center", color: "lime" },
+          { name: "Add Date", alignment: "center", color: "cyan" },
+        ],
+    });
+    for (const usrGrade of usrGrades){
+        vp.addRow(
+            { 
+                Grade: usrGrade["Grade"], 
+                Lesson: usrGrade["Subject"], 
+                Teacher: usrGrade["Teacher"], 
+                "Add Date": usrGrade["Date"],
+            }
+        );
+    }
+    vp.printTable()
+}
+
+welcome();
+
+api.authUsername(login, pass).then(async function (token) {
+    if(token == "Bearer undefined"){
+        console.error("Invalid Login or Password")
+        console.error("Please edit default data on config.js")
+    }
+    else{
+        await getGradesTable(token);
+    }
+
+});
+
+
+
+
+
