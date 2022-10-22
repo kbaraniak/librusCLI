210b3e173521abe70671b35c4743f55f5d0de5eb
Klubuntu <local@fix.pc>
2022-10-22 23:02:30 +0200

    Update Main Program

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const { Table } = require("console-table-printer");
+const fs = require("fs");
+const var_dump = require("var_dump");
+let login, pass;
+
 const Librus = require('./API/api')
-
-const { login, pass } = require("./config");
-
 const api = new Librus();
 
 function welcome(){
     console.log("Librus CLI v0.2\n\n")
     console.log("Loading Default Option - Available Grades Table")
+}
+
+function checkConfig(){
+    if (fs.existsSync("./config.js")) {
+        let { login, pass } = require("./config");
+    }
+    else{
+        var configFile = fs.createWriteStream("./config.js");
+        configFile.write(
+'var config= {\n\
+    login: "login-synergia",\n\
+    pass: "haslo-synergia"\n\
+}\n\
+\n\
+module.exports = config'
+        );
+        configFile.end();
+        console.log("No Found config file, creating default")
+    }
 }
 
 async function getGradesTable(token){
@@ -22,20 +42,27 @@ async function getGradesTable(token){
           { name: "Add Date", alignment: "center", color: "cyan" },
         ],
     });
-    for (const usrGrade of usrGrades){
-        vp.addRow(
-            { 
-                Grade: usrGrade["Grade"], 
-                Lesson: usrGrade["Subject"], 
-                Teacher: usrGrade["Teacher"], 
-                "Add Date": usrGrade["Date"],
-            }
-        );
+    try{
+        for (const usrGrade of usrGrades){
+            vp.addRow(
+                { 
+                    Grade: usrGrade["Grade"], 
+                    Lesson: usrGrade["Subject"], 
+                    Teacher: usrGrade["Teacher"], 
+                    "Add Date": usrGrade["Date"],
+                }
+            );
+        }
+        vp.printTable()
+    }catch(e){
+        var_dump(e);
+        console.error("Debugger Error Data [Grades API - display Table]:")
+        var_dump(usrGrades);
     }
-    vp.printTable()
 }
 
 welcome();
+checkConfig();
 
 api.authUsername(login, pass).then(async function (token) {
     if(token == "Bearer undefined"){
