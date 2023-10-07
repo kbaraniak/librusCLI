f1dda8a357e9afd0d814a7b982f3d245df74ca32
Kamil (Klubuntu) <kontakt@klubuntu.social>
2023-10-08 01:18:36 +0200

    New features for release v1.1

--- app.js
+++ app.js
@@ -2,40 +2,103 @@ const { Table } = require("console-table-printer");
 const fs = require("fs");
 const var_dump = require("var_dump");
 const strikeText = require("./external/strike");
-const strings = require('node-strings');
+const strings = require("node-strings");
 
 const { LibrusAPI } = require("./api/api");
 const api = new LibrusAPI();
+const appVersion = "v1.0 Beta 2";
+let debug;
 
 function welcome() {
-  console.log("Librus CLI v1.0 Beta 1\n\n");
-  console.log("");
+  console.log(`Librus CLI ${appVersion}`);
 }
 
-function checkConfig() {
+async function checkConfig() {
   if (fs.existsSync("./config.js")) {
     try {
-      let { login, pass } = require("./config");
-      return { login, pass };
-    } catch {
-      console.log(
-        "[LibrusCLI] Invalid config file, please delete and rerun program"
-      );
+      let { login, pass, debug } = require("./config");
+      if (!(typeof debug === "undefined")) {
+        console.log("[LibrusCLI] Debug enabled");
+      }
+      nextStep(login, pass, debug);
+      return debug
+    } catch (e) {
+      console.log(e)
+      console.log("[LibrusCLI] Invalid config file, please delete and rerun program");
       process.exit(1);
     }
   } else {
+    const readline = require("readline");
+    let login,
+      pass,
+      debug = undefined;
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    console.log("Librus Synergia:");
+
+    const authLogin = () => {
+      return new Promise((resolve) => {
+        rl.question("Login: ", (answer) => {
+          resolve(answer);
+        });
+      });
+    };
+
+    const authPassword = () => {
+      return new Promise((resolve) => {
+        rl.question("Password: ", (answer) => {
+          resolve(answer);
+        });
+        rl._writeToOutput = function _writeToOutput() {
+          rl.output.write("*");
+        };
+      });
+    };
+
+    const firstLogin = async () => {
+      login = await authLogin();
+      pass = await authPassword();
+      rl.close();
+      return { login, pass, debug };
+    };
+
+    const checkNewConfig = async () => {
+      try {
+        const configFile = await fs.promises.readFile("./config.js");
+        const config = JSON.parse(configFile);
+        return config;
+      } catch (error) {
+        return {};
+      }
+    };
+    const { login: loginFromConfig, pass: passFromConfig } =
+      await checkNewConfig();
+
+    // If the login and pass are not defined in the config file, prompt the user for them.
+    if (!loginFromConfig || !passFromConfig) {
+      const { login, pass } = await firstLogin();
+      console.log(login, pass);
+    } else {
+      login = loginFromConfig;
+      pass = passFromConfig;
+    }
     var configFile = fs.createWriteStream("./config.js");
     configFile.write(
-      'var config= {\nlogin: "login-synergia",\npass: "password-synergia"\n}\n\nmodule.exports = config'
+      `var config= {\nlogin: "${login}",\npass: "${pass}"\n}\n\nmodule.exports = config`
     );
     configFile.end();
+
     console.log("[LibrusCLI] No Found config file, creating new config");
-    console.log(
-      "[LibrusCLI] Created new config, please fill with your data and restart program"
-    );
-    return false;
+    console.log("[LibrusCLI] Created new config, loading menu ...");
+    console.clear()
+    nextStep(login, pass, debug)
   }
 }
+
 
 function removeEmptyElements(obj) {
   const newObj = {};
@@ -47,12 +110,12 @@ function removeEmptyElements(obj) {
   return newObj;
 }
 
-function fixURL(url){
+function fixURL(url) {
   return url.replaceAll("https://api.librus.pl/2.0/", "");
 }
 
 async function getGradesTable() {
-  console.log("Loading Grades ...")
+  console.log("Loading Grades ...");
   const Grades = await api.getGrades().then((data) => {
     return data.Grades;
   });
@@ -66,16 +129,20 @@ async function getGradesTable() {
   });
   try {
     for (const grade of Grades) {
-      const lessonGrade = await api.getSubjects(grade["Subject"].Id).then((data) => {
-        return data.Subject;
-      });
-      const authorGrade = await api.getUsers(grade["AddedBy"].Id).then((data) => {
-        return data.User;
-      });
+      const lessonGrade = await api
+        .getSubjects(grade["Subject"].Id)
+        .then((data) => {
+          return data.Subject;
+        });
+      const authorGrade = await api
+        .getUsers(grade["AddedBy"].Id)
+        .then((data) => {
+          return data.User;
+        });
       gradesTable.addRow({
         Grade: grade["Grade"],
         Lesson: lessonGrade["Short"],
-        Teacher: authorGrade["FirstName"] + " "  + authorGrade["LastName"],
+        Teacher: authorGrade["FirstName"] + " " + authorGrade["LastName"],
         "Add Date": grade["Date"],
       });
     }
@@ -86,25 +153,53 @@ async function getGradesTable() {
     console.error("Debugger Error Data [Grades API - display Table]:");
     var_dump(Grades);
   }
+  backtoMenu();
 }
 
+async function experimentalPointsTable(debug=false) {
+  console.log("Loading Points Grades ...");
+  const Grades = await api.getPointGrades().then((data) => {
+    return data.Grades;
+  });
+  if (debug) {
+    let sep = "==================================";
+    console.log("Debugging Data:");
+    console.log(
+      strings.italic(
+        "If you want to support the project, copy the data between '=' and create a new issue"
+      )
+    );
+    console.log(
+      strings.italic(
+        strings.bold(
+          "New Issue: https://github.com/kbaraniak/librusCLI/issues/new/choose"
+        )
+      )
+    );
+    console.log(sep);
+    console.log(Grades);
+    console.log(sep);
+  }
+  backtoMenu();
+}
 
-async function getTimetables(oneDay=false, week="") {
+async function getTimetables(oneDay = false, week = "") {
   const Timetable = await api.getTimetables().then(async (data) => {
-    // console.log(data)
-    if(week == "next"){
-      let nextTimetable = await api.getAPI(fixURL(data.Pages.Next)).then((data) => {
-        return data.Timetable;
-      });
+    if (week == "next") {
+      let nextTimetable = await api
+        .getAPI(fixURL(data.Pages.Next))
+        .then((data) => {
+          return data.Timetable;
+        });
       return nextTimetable;
-    }
-    else if(week == "prev"){
-      let prevTimetable = await api.getAPI(fixURL(data.Pages.Prev)).then((data) => {
-        return data.Timetable;
-      });
+    } else if (week == "prev") {
+      let prevTimetable = await api
+        .getAPI(fixURL(data.Pages.Prev))
+        .then((data) => {
+          return data.Timetable;
+        });
       return prevTimetable;
-    }
-    else{
+    } else {
       return data.Timetable;
     }
   });
@@ -128,8 +223,9 @@ async function getTimetables(oneDay=false, week="") {
       if (i) {
         const filteredData = removeEmptyElements(i);
         if (Object.keys(filteredData).length > 0) {
-          if(!oneDay){
-            let Time = filteredData[0].HourFrom + " - " + filteredData[0].HourTo;
+          if (!oneDay) {
+            let Time =
+              filteredData[0].HourFrom + " - " + filteredData[0].HourTo;
             let LessonName = filteredData[0].Subject.Short;
             if (filteredData[0].IsCanceled) {
               LessonName = strikeText(LessonName) + " (Canceled)";
@@ -142,18 +238,18 @@ async function getTimetables(oneDay=false, week="") {
                 " " +
                 filteredData[0].Teacher.LastName,
             });
-          }
-          else{
+          } else {
             const getTodayNumber = () => {
               const today = new Date();
               const dayOfWeek = today.getDay();
-            
+
               // Monday is 1, Sunday is 0
               return Number(dayOfWeek);
             };
-            function printData(filteredData){
-              let Time = filteredData[0].HourFrom + " - " + filteredData[0].HourTo;
-              let LessonName = filteredData[0].Subject.Short
+            function printData(filteredData) {
+              let Time =
+                filteredData[0].HourFrom + " - " + filteredData[0].HourTo;
+              let LessonName = filteredData[0].Subject.Short;
               if (filteredData[0].IsCanceled) {
                 LessonName = strikeText(LessonName) + " (Canceled)";
               }
@@ -167,11 +263,13 @@ async function getTimetables(oneDay=false, week="") {
               });
             }
             const dayNumber = getTodayNumber();
-            if(oneDay == "Today" && filteredData[0].DayNo == dayNumber){
-              printData(filteredData)
-            }
-            else if(oneDay == "Tomorrow" && filteredData[0].DayNo == dayNumber+1){
-              printData(filteredData)
+            if (oneDay == "Today" && filteredData[0].DayNo == dayNumber) {
+              printData(filteredData);
+            } else if (
+              oneDay == "Tomorrow" &&
+              filteredData[0].DayNo == dayNumber + 1
+            ) {
+              printData(filteredData);
             }
           }
         }
@@ -187,36 +285,61 @@ async function getTimetables(oneDay=false, week="") {
 
     day++;
   }
+  backtoMenu();
 }
 
-function aboutChangelog(){
-    console.log("")
-    console.log(strings.bold(strings.underline("Changelog\n")))
-    console.log(strings.underline("Version v1.1"));
-    console.log("> Optimized CLI code");
-    console.log("> Updated code for latest API");
-    console.log("> Changed authentication method");
-    console.log(strings.underline("Version v0.4"));
-    console.log("> Update for Timetables");
-    console.log("> Add menu options");
-    console.log("> Add Changelog and check updates");
-    console.log("--------------------------------------------------");
-    console.log("Check latest version on:");
-    console.log("https://github.com/kbaraniak/librusCLI/releases/latest");
-    console.log("See you on the next time");
+function backtoMenu() {
+  const selectOption = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  console.log("\n0. Exit");
+  console.log("Other key - Back to Menu");
+  selectOption.question("> ", async function (arg) {
+    if (Number(arg) == 0 && arg.length > 0) {
+      process.exit(0);
+    } else {
+      console.clear();
+      selectOption.close();
+      showMenu();
+    }
+  });
 }
-function aboutAuthor(){
+
+function aboutChangelog() {
+  console.log("");
+  console.log(strings.bold(strings.underline("Changelog\n")));
+  console.log(strings.underline("Version v1.0"));
+  console.log("> Optimized CLI code");
+  console.log("> Updated code for latest API");
+  console.log("> Changed authentication method");
+  console.log(strings.underline("Version v0.4"));
+  console.log("> Update for Timetables");
+  console.log("> Add menu options");
+  console.log("> Add Changelog and check updates");
+  console.log("--------------------------------------------------");
+  console.log("Check latest version on:");
+  console.log("https://github.com/kbaraniak/librusCLI/releases/latest");
+  console.log("See you on the next time");
+
+  backtoMenu();
+}
+function aboutAuthor() {
   console.log(strings.underline("\nAbout Author:"));
   console.log("Author: Kamil Baraniak");
   console.log("Github: https://github.com/kbaraniak");
-  console.log("[LibrusCLI v1.1] - Thank you for usage");
+  console.log(`[LibrusCLI ${appVersion}] - Thank you for usage`);
+
+  backtoMenu();
 }
 
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-async function menu(token) {
+async function showMenu(debug) {
+  const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  console.clear();
+  welcome();
   console.log(strings.bold("Select option\n"));
   console.log(strings.underline("Grades:"));
   console.log("- 1. Show grades\n");
@@ -226,15 +349,18 @@ async function menu(token) {
   console.log("- 4. Show timetable for prev week");
   console.log("- 5. Show timetable only for Today");
   console.log("- 6. Show timetable only for Tomorrow\n");
+  if (debug) {
+    console.log(strings.underline("Experimental:"));
+    console.log("- 91. Show point grades\n");
+  }
   console.log(strings.underline("About:"));
   console.log("- 0. Changelog");
   console.log("- A. Author");
-
-  readline.question("> ", async function (arg) {
-    if ((arg.length < 0 && isNaN(parseInt(arg)))) {
+  console.log("- X. Exit App");
+  readline.question("Select > ", async function (arg) {
+    if (arg.length < 0 && isNaN(parseInt(arg))) {
       readline.close();
-      console.log("");
-      menu();
+      showMenu(debug);
     } else if (arg.length < 0) {
       console.log("Selected default option");
       await getTimetables();
@@ -248,31 +374,38 @@ async function menu(token) {
       } else if (opt == 2) {
         await getTimetables();
       } else if (opt == 3) {
-        await getTimetables(oneDay=false, week="next");
+        await getTimetables((oneDay = false), (week = "next"));
       } else if (opt == 4) {
-        await getTimetables(oneDay="Today")
+        await getTimetables((oneDay = false), (week = "prev"));
       } else if (opt == 5) {
-        await getTimetables(oneDay="Tomorrow")
-      } else if (arg == "A" || arg == "a"){
+        await getTimetables((oneDay = "Today"));
+      } else if (opt == 6) {
+        await getTimetables((oneDay = "Tomorrow"));
+      } else if (opt == 91 && debug) {
+        await experimentalPointsTable(debug);
+      } else if (arg == "A" || arg == "a") {
         aboutAuthor();
-      } else {
-        console.log("Selected default option");
-        await getTimetables();
+      } else if (arg == "x" || arg == "x") {
+        process.exit(0);
+      }
+      else{
+        showMenu(debug)
       }
     }
   });
 }
 
 /* Main Function */
-welcome();
-let { login, pass } = checkConfig();
-api.mkToken(login, pass).then((r) => {
-  if (r.status == "error") {
-    console.error("[LibrusCLI] Invalid Login or Password");
-    console.error(
-      "[LibrusCLI] Please check for correct login/password in config.js"
-    );
-    process.exit(1);
-  }
-  menu()
-});
+checkConfig()
+
+function nextStep(login,pass, debug){
+  api.mkToken(login, pass).then((r) => {
+    if (r.status === 'error') {
+      console.error('[LibrusCLI] Invalid Login or Password');
+      console.error('[LibrusCLI] Please check for correct login/password in config.js');
+      process.exit(1);
+    }
+    console.log("[LibrusCLI] User authentication. Please wait ...\n\n")
+    showMenu(debug);
+  });
+}
